a69f5ec1475a65174300e9f29819c7e115f5a31d
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-03-06 12:25:28 +0100

    Updated redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {themeSlice} from "@/redux/slices/themeSlice";
 
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        theme: themeSlice.reducer
+    },
 })
 
 
