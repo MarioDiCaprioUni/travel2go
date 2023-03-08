8c612e1eac0a452a86d3ca7bd34b77203cc4b41c
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-03-08 10:48:51 +0100

    Saves theme in localStorage

--- src/redux/slices/themeSlice.ts
+++ src/redux/slices/themeSlice.ts
@@ -3,13 +3,24 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 type ThemeState = "light" | "dark" | "device";
 
+let initialState: ThemeState = "device";
+if (typeof window !== 'undefined') {
+    let tmp = localStorage.getItem("theme") as ThemeState | null;
+    if (tmp) {
+        initialState = tmp;
+    }
+}
+
 
 export const themeSlice = createSlice({
     name: "theme",
-    initialState: "device" as ThemeState,
+    initialState,
     reducers: {
 
         setTheme: (_state, payload: PayloadAction<ThemeState>) => {
+            if (typeof window !== 'undefined') {
+                localStorage.setItem('theme', payload.payload);
+            }
             return payload.payload;
         }
 
