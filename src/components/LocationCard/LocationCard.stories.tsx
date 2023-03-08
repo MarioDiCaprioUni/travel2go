05e2059209d35a49c4f8b51876ddb9285562f9d2
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-03-08 20:50:31 +0100

    Fixed error with props

--- src/components/LocationCard/LocationCard.stories.tsx
+++ src/components/LocationCard/LocationCard.stories.tsx
@@ -19,8 +19,12 @@ export const Main: StoryFn<typeof LocationCard> = (props) => {
 }
 
 Main.args = {
-    thumbnailLink: 'https://strongcitiesnetwork.org/en/wp-content/uploads/sites/5/2017/11/Berlin-Nikolaiviertel-scaled.jpg',
-    title: 'Berlin, Germany',
-    tooltip: 'Berlin',
-    tags: ['Germany', 'European', 'Culture']
+    title: "Berlin, Germany",
+    tooltip: "Brandenburg Gate",
+    thumbnailLink: "https://images.unsplash.com/photo-1587330979470-3595ac045ab0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGJlcmxpbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    dailyEuros: 3,
+    coords: {
+        lat: 52.5200,
+        lon: 13.4050
+    }
 }
